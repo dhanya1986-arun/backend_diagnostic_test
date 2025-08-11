a63fe15dc23c8670b52441bfc3f064822ff66e97
dhanya1986-arun <dhanyaarun.c@gmail.com>
2025-08-11 12:46:19 +0530

    Fix backend deployment and CORS config

--- server.js
+++ server.js
@@ -5,21 +5,27 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require("path");
 
-
 dotenv.config();
+//MONGO_URI=mongodb://localhost:27017/diagnostic_test
+//MONGO_URI=mongodb+srv://dhanya_joseph:dhanya86@cluster0.hvdgiue.mongodb.net/diagnostic_test
+//MONGO_URI=mongodb+srv://dhanya_joseph:dhanya86@cluster0.hvdgiue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 const app = express();
 app.use(cors());
 app.use(express.json());
 console.log("Mongo URI:2", process.env.MONGO_URI);
 // DB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI, {dbName : "diagnostic_test"} 
+  )
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.error("MongoDB error:", err));
 
 // Routes
+app.use(cors({
+    origin: 'http://localhost:3000', // frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+app.use(express.json());
 
 const authRoutes= require('./routes/authroutes');
 const orderRoutes = require('./routes/orderRoutes')
